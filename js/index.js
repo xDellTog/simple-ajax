c028bca592147596645c00619ecc2c48567b7cb4
Tognon <xone.tognon@outlook.com>
2018-04-03 09:35:39 -0300

    update #3

--- js/index.js
+++ js/index.js
@@ -1,33 +1,41 @@
 'use strict';
 
-(function() {
+(
+	function() {
+		console.log('ready!');
 
-	$('#btnGetHtml').onclick = function(ev) {
-		ev.preventDefault();
-		let ajax = new Ajax({
-			type: 'GET',
-			url: '../php/main.php',
-			dataType: 'html',
-			async: false,
-			body: {
-				nome: 'Daniel',
-				senha: 'Senha',
-				sexo: 'Masculino'
-			}
-		});
-	}
-	$('#btnGetJson').onclick = function (ev) {
-		ev.preventDefault();
-		let ajax = new Ajax({
-			type: 'GET',
-			url: '../php/main.php',
-			dataType: 'json',
-			async: false,
-			body: {
-				nome: 'Daniel',
-				senha: 'Senha',
-				sexo: 'Masculino'
-			}
-		});
-	}
-}());
+		let btnGetHtml = $('#btnGetHtml');
+		let btnGetJson = $('#btnGetJson');
+
+		btnGetHtml.onclick = function(ev) {
+			ev.preventDefault();
+			console.log('clicked');
+			let ajax = new Ajax({
+				type: 'GET',
+				url: '../php/main.php',
+				dataType: 'html',
+				async: false,
+				body: {
+					nome: 'Daniel',
+					senha: 'Senha',
+					sexo: 'Masculino'
+				}
+			});
+			console.dir(ajax.response);
+		}
+		btnGetJson.onclick = function (ev) {
+			ev.preventDefault();
+			let ajax = new Ajax({
+				type: 'GET',
+				url: '../php/main.php',
+				dataType: 'json',
+				async: false,
+				body: {
+					nome: 'Daniel',
+					senha: 'Senha',
+					sexo: 'Masculino'
+				}
+			});
+		}
+	}()
+);
