e231755c8e521b16988be2d6b48ea3e06c7ca3c3
Daniel Tognon <37959139+xDellTog@users.noreply.github.com>
2018-05-01 15:02:18 -0300

    Update ajax.js #3

--- js/ajax.js
+++ js/ajax.js
@@ -2,25 +2,34 @@
 
 class Ajax {
 	constructor({url = null, body = null, async = false, type = 'GET', dataType = 'HTML'} = {}) {
+		this.url = url;
+		this.body = body;
+		this.async = async;
+		this.type = type;
+		this.dataType = dataType;
 		this.response = {};
 		this.xhr = new XMLHttpRequest();
+		
+		this.xhrOpen(this.xhr);
+	}
 
-		if (url !== null) {
-			this.xhr.open(type, url, async);
-			if (type === 'GET') {
-				this.xhr.send();
-				this.response = this.getResponse(arguments, this.xhr);
-			} else if (type === 'POST') {
-				this.xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-				this.xhr.send(this.transform(body));
-				this.response = this.getResponse(arguments, this.xhr);
-			}	
+	xhrOpen(xhr) {
+		xhr.open(this.type, this.url, this.async);
+		if (this.isGet(this.type)) {
+			this.xhr.send();
+			this.response = this.getResponse(this.xhr);
 		} else {
-			throw new Error('URL null');
+			this.xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			this.xhr.send(this.getBody(this.body));
+			this.response = this.getResponse(this.xhr);
 		}
 	}
 
-	transform(body) {
+	isGet(type) {
+		return (type === 'GET') ? true : false;
+	}
+
+	getBody(body) {
 		let aux = JSON.stringify(body);
 		aux = aux.replace(/:/g, '=');
 		aux = aux.replace(/,/g, '&');
@@ -30,16 +39,14 @@ class Ajax {
 		return aux;
 	}
 
-	getResponse(args, ...xhr) {
-		let arg = args[0];
-		
-		if ((arg.dataType.toUpperCase() === 'TEXT') || (arg.dataType.toUpperCase() === 'HTML')) {
+	getResponse(xhr) {
+		if ((this.dataType.toUpperCase() === 'TEXT') || (this.dataType.toUpperCase() === 'HTML')) {
 			if ((this.xhr.readyState === 4) && (this.xhr.status === 200)) {
 				return this.xhr.responseText;
 			} else {
 				throw new Error('Code ' + this.xhr.status);
 			}
-		} else if (arg.dataType.toUpperCase() === 'JSON') {
+		} else if (this.dataType.toUpperCase() === 'JSON') {
 			if ((this.xhr.readyState === 4) && (this.xhr.status === 200)) {
 				return JSON.parse(this.xhr.responseText);
 			} else {
